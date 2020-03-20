ce5048358d611f76da07ae20b3098ac212fafddf
Mitchell Bonham <mitchell.r.bonham@gmail.com>
2020-03-20 14:27:33 +1100

    Vector2 class

---
+++ MathClasses/Vector2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClasses
+{
+    class Vector2
+    {
+        public float x, y;
+
+        public Vector2()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public Vector2(float _x, float _y)
+        {
+            x = _x;
+            y = _y;
+        }
+    }
+}
