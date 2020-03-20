7670ad5a1f875aa56d68f39996efb703ad9d9582
Mitchell Bonham <mitchell.r.bonham@gmail.com>
2020-03-20 11:45:04 +1100

    Vector3 scaling

--- MathClasses/Vector3.cs
+++ MathClasses/Vector3.cs
@@ -34,5 +34,15 @@ namespace MathClasses
             return new Vector3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
         }
 
+        public static Vector3 operator *(Vector3 v, float f)
+        {
+            return new Vector3(v.x * f, v.y * f, v.z * f);
+        }
+
+        public static Vector3 operator *(float f, Vector3 v)
+        {
+            return new Vector3(v.x * f, v.y * f, v.z * f);
+        }
+
     }
 }
